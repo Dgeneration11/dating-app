8bfcb5c964f8f0f059af080c942f1ab3e1ad68e6
= <buntydgeneration@gmail.com>
2021-08-18 20:00:56 +0530

    header

--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,17 @@ import IconButton from '@material-ui/core/IconButton'
 function Header() {
     return (
         <div className='header'>
-            <h2> I am the Header</h2>
+            {/* <h2> I am the Header</h2> */}
             <IconButton>
                 <PersonIcon fontSize="large" className='personIcon' />
             </IconButton>
+
+            <img
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/Tinder_logo_PNG2.png"
+                alt=""
+            />
+
         </div>
     )
 }
