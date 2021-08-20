fd03310c8678b65ecd78892230a4a020b0e9ed7d
= <buntydgeneration@gmail.com>
2021-08-21 00:18:13 +0530

    New

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,45 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cards from './dbCards.js';
 
 //App config
 const app = express();
 const PORT = process.env.PORT || 8001;
-
+const connection_url = `mongodb+srv://dgen:dgen@cluster0.isacn.mongodb.net/datingDB?retryWrites=true&w=majority`
 
 // Middllwares
 
+
 // DB config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send("Hello Bunty !!"));
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+    cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/tinder/cards', (req, res) => {
+    cards.find({}, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
 
 
 // Listener
